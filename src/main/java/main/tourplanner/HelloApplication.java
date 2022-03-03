8bc20743a9b5b975b115eef8f0e50ff5142ebc81
Manuel T <manuelthuer72@gmail.com>
2022-03-03 18:52:53 +0100

    Simple layout / user interface idea by Manuel

--- src/main/java/main/tourplanner/HelloApplication.java
+++ src/main/java/main/tourplanner/HelloApplication.java
@@ -10,9 +10,13 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello Tour Planner Groupe!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-page.fxml"));
+        //change here
+        Scene scene = new Scene(fxmlLoader.load(), 500,500);
+        stage.setTitle("Tour Planner Test, Yeah!");
+        stage.setMinHeight(400);
+        stage.setMinWidth(400);
+        //change here
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/main/tourplanner/HelloController.java
+++ src/main/java/main/tourplanner/HelloController.java
@@ -1,14 +1,23 @@
 package main.tourplanner;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Label testTextObjectPlaceholder;
+
+    public void onRouteButtonClick(ActionEvent actionEvent) {
+        testTextObjectPlaceholder.setText("here you can see the route aka map, Yeah!");
+    }
+
+    public void onDescriptionButtonClick(ActionEvent actionEvent) {
+        testTextObjectPlaceholder.setText("here you can see the description of the route, Yippie!\nfrom:\nto:\ntransport type:\ntour distance:\nestimated time:\n");
+    }
+
+    public void onLogsButtonClick(ActionEvent actionEvent) {
+        testTextObjectPlaceholder.setText("here you can see your logs and logs of other users, Yesssa! (in a table)\n\tdate:\n\ttime:\n\tdistance:");
     }
 }
