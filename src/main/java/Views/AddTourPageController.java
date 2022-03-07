2f8a8c5645773203dcb74ba4bfdec92483af9683
Manuel T <manuelthuer72@gmail.com>
2022-03-07 19:58:40 +0100

    Error

---
+++ src/main/java/Views/AddTourPageController.java
@@ -0,0 +1,31 @@
+package Views;
+
+import Models.AddTourPageModel;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+
+public class AddTourPageController {
+
+    private AddTourPageModel model = new AddTourPageModel();
+
+    @FXML
+    private TextField tourNameInputTextField;
+    @FXML
+    public Label tourNameOutputLabel;
+
+    @FXML
+    public void initialize(){
+
+        this.tourNameInputTextField.textProperty().bindBidirectional(model.getTourNameInputProperty());
+        this.tourNameOutputLabel.textProperty().bindBidirectional(model.getTourNameOutputProperty());
+
+    }
+
+    public void addTourNameOnClick(ActionEvent actionEvent){
+        this.model.concat();
+    }
+
+}
--- src/main/java/Views/TourPlannerController.java
+++ src/main/java/Views/TourPlannerController.java
@@ -1,10 +1,18 @@
 package Views;
 
+import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
-public class TourPlannerController {
+import java.io.IOException;
+
+public class TourPlannerController  {//extends Application
+
+    private TourPlannerApplication tpa = new TourPlannerApplication();
 
     @FXML
     private Label testTextObjectPlaceholder;
@@ -20,4 +28,26 @@ public class TourPlannerController {
     public void onLogsButtonClick(ActionEvent actionEvent) {
         testTextObjectPlaceholder.setText("here you can see your logs and logs of other users, Yesssa! (in a table)\n\tdate:\n\ttime:\n\tdistance:");
     }
+
+    public void addTourOnButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        tpa.addRoute(stage);
+
+        /*try {
+            this.start(stage);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }*/
+    }
+
+    /*@Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(TourPlannerApplication.class.getResource("add-tour-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500,500);
+        stage.setTitle("Tour Planner Test, Yeah!");
+        stage.setMinHeight(400);
+        stage.setMinWidth(400);
+        stage.setScene(scene);
+        stage.show();
+    }*/
 }
