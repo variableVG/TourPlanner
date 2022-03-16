9351c55ca1f656f914acae90d6aaceeeb37163f8
RawanMousa9896 <rawan.mousa.at@gmail.com>
2022-03-16 20:11:24 +0100

    log tab einfuegen

--- src/main/java/Views/LogTourTabController.java
+++ src/main/java/Views/LogTourTabController.java
@@ -1,4 +1,15 @@
 package Views;
 
+import javafx.event.ActionEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class LogTourTabController {
+    private TourPlannerApplication tpa = new TourPlannerApplication();
+
+    public void addLogOnButtonClick(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        tpa.addLog(stage);
+    }
 }
--- src/main/java/Views/TourPlannerApplication.java
+++ src/main/java/Views/TourPlannerApplication.java
@@ -30,6 +30,17 @@ public class TourPlannerApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
+    public void addLog(Stage stage) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(TourPlannerApplication.class.getResource("add-log-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 500,500);
+        stage.setTitle("Tour Planner Test, Yeah!");
+        stage.setMinHeight(400);
+        stage.setMinWidth(400);
+        stage.setX(600);
+        stage.setY(150);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public static void main(String[] args) {
         launch();
