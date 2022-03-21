e0db00cbc5323bda4866a9242155d9de31ce69a3
RawanMousa9896 <rawan.mousa.at@gmail.com>
2022-03-21 19:38:27 +0100

    create log and tour class

---
+++ src/main/java/Models/Log.java
@@ -0,0 +1,11 @@
+package Models;
+
+public class Log {
+    public String date;
+    public String time;
+    public String comment;
+    public int difficulty;
+    public double totaltime;
+    public int rating;
+
+}
---
+++ src/main/java/Models/Tour.java
@@ -0,0 +1,19 @@
+package Models;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Tour {
+    private String name;
+    private String description;
+    private String origin;
+    private String destination;
+    private String transportType;
+    private double distance;
+    private double time;
+    private String information;
+
+    private ArrayList<Log> logs;
+
+
+}
