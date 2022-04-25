94944e9576498d27a58dbf97a5c0b956da75c486
Manuel T <manuelthuer72@gmail.com>
2022-04-25 20:50:32 +0200

    delete window, cancel button closes window

--- src/main/java/Views/DeleteTourPageController.java
+++ src/main/java/Views/DeleteTourPageController.java
@@ -1,14 +1,22 @@
 package Views;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class DeleteTourPageController {
 
-    //@FXML Label ...
+    @FXML public Button cancelButton;
+    //@FXML public Button confirmDeleteButton;
 
     public DeleteTourPageController() throws IOException {
+    }
 
+    public void onCancelButtonClick(ActionEvent event) {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
     }
 }
