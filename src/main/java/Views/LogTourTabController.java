cc16f73dd91c33494654aa7e984b57d5f56da272
Violeta <vio.garciaespin@gmail.com>
2022-05-16 14:40:15 +0200

    show logs - needs improvement with user interface

--- src/main/java/Views/LogTourTabController.java
+++ src/main/java/Views/LogTourTabController.java
@@ -6,53 +6,133 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
 public class LogTourTabController {
 
-
-    @FXML public TableColumn comment;
-    @FXML public TableColumn difficulty;
-    @FXML public TableColumn total_time;
-    @FXML public TableColumn rating;
-    @FXML public TableColumn log_id;
-    @FXML public TableColumn date;
-    @FXML public TableColumn time;
+    public javafx.scene.layout.VBox VBox;
+    /*@FXML public TableView<Log> tableViewLogs;
+        @FXML public TableColumn comment;
+        @FXML public TableColumn difficulty;
+        @FXML public TableColumn total_time;
+        @FXML public TableColumn rating;
+        @FXML public TableColumn log_id;
+        @FXML public TableColumn date;
+        @FXML public TableColumn time;*/
     @FXML private TourPlannerApplication tpa = new TourPlannerApplication();
     Tour tour;
 
-    @FXML public TableView<String> tableViewLogs;
+
 
     //constructor for controller factory
 
     LogTourTabController(Tour tour) {
         this.tour = tour;
+    }
 
+    @FXML
+    public void initialize(){
 
     }
 
     public void updateLogs(Tour tour) {
-        this.tour = tour;
-        //tableViewLogs.setItems(tour.getLogs());
-        ObservableList<String> logs = FXCollections.observableArrayList();
-        // Source: https://www.tutorialspoint.com/how-to-add-data-to-a-tableview-in-javafx
-        for(Log l : tour.getLogs()) {
-            System.out.println("Hi?");
-            logs.add(String.valueOf(l.getId()));
-            logs.add(l.getDate().getValue());
-            logs.add(l.getTime().getValue());
-            logs.add(l.getComment().getValue());
-            logs.add(String.valueOf(l.getDifficulty()));
-            logs.add(l.getTotaltime().getValue());
-            logs.add(String.valueOf(l.getRating()));
-            tableViewLogs.setItems(logs);
+        VBox.getChildren().clear();
+        String labelTitleStyle = "-fx-font-weight: bold;";
+        String cssLayout = "-fx-border-color: grey;\n" +
+                "-fx-border-insets: 5;\n" +
+                "-fx-border-width: 2;\n" +
+                "-fx-border-style: dashed;\n";
+
+        for (Log l : tour.getLogs()) {
+            VBox logBox = new VBox();
+            logBox.setPadding(new Insets(10));
+
+            //ID BOX
+            HBox idBox = new HBox();
+            Label idLabel = new Label();
+            idLabel.setText("Log id:");
+            idLabel.setStyle(labelTitleStyle);
+            Label idLabelContent = new Label();
+            idLabelContent.setText("  " + String.valueOf(l.getId().getValue()));
+            idBox.getChildren().addAll(idLabel, idLabelContent);
+            logBox.getChildren().addAll(idBox);
+
+            //DATE AND TIME
+            HBox dateBox = new HBox();
+            Label dateLabel = new Label();
+            dateLabel.setText("Date: ");
+            dateLabel.setStyle(labelTitleStyle);
+            Label dateLabelContent = new Label();
+            dateLabelContent.setText(" " + l.getDate().getValue());
+            Label timeLabel = new Label();
+            timeLabel.setText(" at " + l.getTime().getValue() + " hours ");
+            dateBox.getChildren().addAll(dateLabel, dateLabelContent, timeLabel);
+            logBox.getChildren().addAll(dateBox);
+
+            //DIFFICULTY
+            HBox difficultyBox = new HBox();
+            Label difficultyLabel = new Label();
+            difficultyLabel.setText("Difficulty: ");
+            difficultyLabel.setStyle(labelTitleStyle);
+            Label difficultyLabelContent = new Label();
+            difficultyLabelContent.setText("  " + String.valueOf(l.getDifficulty().getValue()));
+            difficultyBox.getChildren().addAll(difficultyLabel, difficultyLabelContent);
+            logBox.getChildren().addAll(difficultyBox);
+
+            //TOTAL TIME
+            HBox totalTimeBox = new HBox();
+            Label totalTimeLabel = new Label();
+            totalTimeLabel.setText("Total time: ");
+            totalTimeLabel.setStyle(labelTitleStyle);
+            Label totalTimeLabelContent = new Label();
+            totalTimeLabelContent.setText("  " + String.valueOf(l.getTotaltime().getValue()));
+            totalTimeBox.getChildren().addAll(totalTimeLabel, totalTimeLabelContent);
+            logBox.getChildren().addAll(totalTimeBox);
+
+            //RATING
+            HBox ratingBox = new HBox();
+            Label ratingLabel = new Label();
+            ratingLabel.setText("Rating: ");
+            ratingLabel.setStyle(labelTitleStyle);
+            Label ratingLabelContent = new Label();
+            ratingLabelContent.setText("  " + String.valueOf(l.getRating().getValue()));
+            ratingBox.getChildren().addAll(ratingLabel, ratingLabelContent);
+            logBox.getChildren().addAll(ratingBox);
+
+            // COMMENTS
+            HBox commentBox = new HBox();
+            Label commentLabel = new Label();
+            commentLabel.setText("Comments: ");
+            commentLabel.setStyle(labelTitleStyle);
+            Label commentLabelContent = new Label();
+            commentLabelContent.setText("  " + String.valueOf(l.getComment().getValue()));
+            commentBox.getChildren().addAll(commentLabel, commentLabelContent);
+            logBox.getChildren().addAll(commentBox);
+
+            // EDIT AND DELETE BUTTONS
+            HBox buttonBox = new HBox();
+            Button editButton = new Button("Edit");
+            editButton.setPadding(new Insets(5));
+            Button deleteButton = new Button("Delete");
+            deleteButton.setPadding(new Insets(5));
+            buttonBox.getChildren().addAll(editButton, deleteButton);
+            logBox.getChildren().addAll(buttonBox);
+
+            logBox.setStyle(cssLayout);
+            VBox.getChildren().addAll(logBox);
         }
+
 
      }
 
