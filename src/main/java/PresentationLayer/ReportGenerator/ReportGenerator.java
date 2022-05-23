8d26e8fee0aa7123ed32ac322af34b8f110c9dec
Manuel T <manuelthuer72@gmail.com>
2022-05-23 22:16:24 +0200

    PDF-Generator: Tour Data and Logs, but no Image

--- src/main/java/PresentationLayer/ReportGenerator/ReportGenerator.java
+++ src/main/java/PresentationLayer/ReportGenerator/ReportGenerator.java
@@ -1,19 +1,18 @@
 package PresentationLayer.ReportGenerator;
 
+import PresentationLayer.Models.Log;
 import com.itextpdf.io.font.constants.StandardFonts;
 import com.itextpdf.io.image.ImageData;
 import com.itextpdf.io.image.ImageDataFactory;
 import PresentationLayer.Models.Tour;
-import com.itextpdf.io.font.constants.StandardFonts;
 import com.itextpdf.kernel.colors.ColorConstants;
 import com.itextpdf.kernel.font.PdfFontFactory;
 import com.itextpdf.kernel.pdf.*;
 import com.itextpdf.layout.Document;
 import com.itextpdf.layout.element.*;
 import com.itextpdf.layout.properties.UnitValue;
-
-import com.itextpdf.layout.Document;
-
+import javax.imageio.ImageIO;
+import java.io.ByteArrayOutputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
@@ -30,31 +29,56 @@ public class ReportGenerator implements IReportGenerator{
         this.writer = new PdfWriter(filename);
         this.pdf = new PdfDocument(writer);
         this.document = new Document(pdf);
-
     }
 
     public void generateReport() throws IOException {
-        Paragraph loremIpsumHeader = new Paragraph("Lorem Ipsum header...")
+        document.add(new Paragraph(tour.getName())
+                .setFont(PdfFontFactory.createFont(StandardFonts.HELVETICA))
+                .setFontSize(18)
+                .setBold()
+                .setFontColor(ColorConstants.BLACK));
+
+        document.add(new Paragraph("From: " + tour.getOrigin() + "\tTo: " + tour.getDestination()));
+        document.add(new Paragraph("Distance: " + tour.getDistance() + "\tTime: " + tour.getTime()));
+        document.add(new Paragraph("TransportType: " + tour.getTransportType() + "\t(some more attributes: ...) "));
+        document.add(new Paragraph("Description: " + tour.getDescription()));
+
+        //convert bufferedimage to bytearray
+        /*ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(tour.getImageMap(), "png", baos);
+        byte[] bytes = baos.toByteArray();
+        ImageData tourImage = ImageDataFactory.create(bytes);
+        document.add(new Image(tourImage));*/
+
+        document.add(new Paragraph("Logs")
                 .setFont(PdfFontFactory.createFont(StandardFonts.HELVETICA))
                 .setFontSize(14)
                 .setBold()
-                .setFontColor(ColorConstants.RED);
-        document.add(loremIpsumHeader);
-        document.add(new Paragraph("Name: " + tour.getName()));
-        document.add(new Paragraph("Description: " + tour.getDescription()));
-        document.add(new Paragraph("Destination: " + tour.getDestination()));
-        document.add(new Paragraph("Origin: " + tour.getOrigin()));
-        document.add(new Paragraph("Distance: " + tour.getDistance()));
-        document.add(new Paragraph("Time: " + tour.getTime()));
-        document.add(new Paragraph("TransportType: " + tour.getTransportType()));
-        document.add(new Paragraph("Logs: " + tour.getLogs()));
-        document.add(new Paragraph("Id: " + tour.getId()));
+                .setFontColor(ColorConstants.BLACK));
 
+        Table logsTable = new Table(UnitValue.createPercentArray(4)).useAllAvailableWidth();
+            logsTable.addHeaderCell(getHeaderCell("Date"));
+            //logsTable.addHeaderCell(getHeaderCell("Time"));
+            logsTable.addHeaderCell(getHeaderCell("Total Time"));
+            logsTable.addHeaderCell(getHeaderCell("Rating"));
+            //logsTable.addHeaderCell(getHeaderCell("Difficulty"));
+            logsTable.addHeaderCell(getHeaderCell("Comment"));
 
-
+        for(Log log : tour.getLogs()){
+            logsTable.addCell(log.getDate().toString());
+            //logsTable.addCell(log.getTime().toString());
+            logsTable.addCell(log.getTotaltime());
+            logsTable.addCell(log.getRating().toString());
+            //logsTable.addCell(log.getDifficulty());
+            logsTable.addCell(log.getComment());
+        }
+        document.add(logsTable);
 
         document.close();
+    }
 
+    private static Cell getHeaderCell(String s) {
+        return new Cell().add(new Paragraph(s)).setBold().setBackgroundColor(ColorConstants.GRAY);
     }
 
 }
